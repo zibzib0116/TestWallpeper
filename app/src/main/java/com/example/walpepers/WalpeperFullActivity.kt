c13f2e0cda609eaa6ed437a8a9c9f6ad76e02b76
zzzzzzzzzzzzzzzzzzzz <cccc@bai.vhj>
2023-02-02 00:23:36 +0400

    addlayouts

---
+++ app/src/main/java/com/example/walpepers/WalpeperFullActivity.kt
@@ -0,0 +1,11 @@
+package com.example.walpepers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalpeperFullActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walpeper_full)
+    }
+}
